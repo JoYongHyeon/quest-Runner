6a4082923a4b9eead02d1694783381f190453753
JoYongHyeon <whdydgus5778@gmail.com>
2026-02-02 12:04:27 +0900

    Initial commit

---
+++ src/main/java/com/questrunner/questrunner/QuestRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.questrunner.questrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestRunnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/questrunner/questrunner/QuestRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questrunner.questrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestRunnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
